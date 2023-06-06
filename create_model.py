2466a12a5bcaca41b85dfbe2e82ee8de9b67a303
Aryan Jain <aryanrashijain@gmail.com>
2023-06-05 19:24:32 -0700

    basic code to create the model

--- create_model.py
+++ create_model.py
@@ -2,4 +2,25 @@ import tensorflow as tf
 from keras.datasets import mnist
 
 
+(train_data, train_labels), (test_data, test_labels) = mnist.load_data()
 
+
+#Simplifying the data
+train_data = train_data / 255
+test_data = test_data / 255
+
+
+model = tf.keras.Sequential({
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(128, activation = 'relu'),
+    tf.keras.layers.Dense(10)
+})
+
+
+model.compile(optimizer = 'adam',
+              loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
+              metrics = ['accuracy'])
+
+model.fit(train_data, train_labels, epochs = 8)
+
+model.save('model.h5')
